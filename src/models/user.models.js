808c803613182d9e41160e5a28f33a8666ec4d13
Abhijeet Gautam <abhijeetgautam572@gmail.com>
2024-01-22 11:54:37 +0530

    Add pre-save-hashing-middleware and methods: token-generation & password-validator

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema(
   {
@@ -50,7 +52,68 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+// MONGOOSE PRE-SAVE-MIDDLEWARE: Hash Password (only if it was modified) before saving a document
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) {
+    return;
+  }
 
+  try {
+    const hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+  } catch (error) {
+    console.log(`PASSWORD HASHING FAILED | ${error}`);
+    throw error;
+  }
+});
+
+// MONGOOSE METHOD: Password Validator
+userSchema.methods.validatePassword = async function (password) {
+  const isPasswordCorrect = await bcrypt.compare(password, this.password);
+  return isPasswordCorrect;
+};
+
+// MONGOOSE METHOD: Access Token Generator
+userSchema.methods.generateAccessToken = async function () {
+  try {
+    const accessToken = await jwt.sign(
+      {
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+      }
+    );
+    return accessToken;
+  } catch (error) {
+    console.log(`ACCESS TOKEN GENERATION FAILED | ${error}`);
+    throw error;
+  }
+};
+
+// MONGOOSE METHOD: Refresh Token Generator
+userSchema.methods.generateAccessToken = async function () {
+  try {
+    const accessToken = await jwt.sign(
+      {
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+      },
+      process.env.REFRESH_TOKEN_SECRET,
+      {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+      }
+    );
+    return accessToken;
+  } catch (error) {
+    console.log(`REFRESH TOKEN GENERATION FAILED | ${error}`);
+    throw error;
+  }
+};
 
 const User = mongoose.model("User", userSchema);
 
