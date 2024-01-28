6c35ad12010cc8730aabfa11a36bfd59c28f4ecf
Abhijeet Gautam <abhijeetgautam572@gmail.com>
2024-01-28 23:02:37 +0530

    Refactored code for better API-Response readability

--- src/utils/customApiResponse.utils.js
+++ src/utils/customApiResponse.utils.js
@@ -7,10 +7,10 @@
 */
 class customApiResponse {
   constructor(message, statusCode, data) {
-    this.message = message;
-    this.statusCode = statusCode;
-    this.data = data;
     this.success = true;
+    this.statusCode = statusCode;
+    this.message = message;
+    this.data = data;
   }
 }
 
