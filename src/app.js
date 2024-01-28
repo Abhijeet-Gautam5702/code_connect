e71f4990bd955cf17aa61f46ad43c88c131c9976
Abhijeet Gautam <abhijeetgautam572@gmail.com>
2024-01-28 23:00:45 +0530

    Add cookie-parser middleware to handle request/response cookies

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
+
+/*
+    `cookie-parser` Middleware to give us the access to the `req.cookies` object and to send cookies to the response object
+*/
+app.use(cookieParser());
 
 // ROUTER IMPORTS
 import userRouter from "./routes/user.routes.js";
