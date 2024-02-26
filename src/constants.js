874cc66212e490dec0cb040a1f8def05c522de2e
Abhijeet Gautam <abhijeetgautam572@gmail.com>
2024-02-26 20:27:21 +0530

    Code refactor: Add initial-error-message constants for all the controllers

--- src/constants.js
+++ src/constants.js
@@ -6,4 +6,16 @@ const API_VERSION = "api/v1";
 
 const CLOUDINARY_CLOUD_NAME = "dmso2v4ec";
 
-export { DB_NAME, API_VERSION ,CLOUDINARY_CLOUD_NAME};
+const INITIAL_ERROR_MESSAGES = {
+  EVENTS: {
+    CREATE_EVENT: "Could not create a new event",
+    REGISTER_EVENT: "Could not register to the event",
+    DELETE_EVENT: "Could not delete the event",
+    DEREGISTER_EVENT: "Could not de-register from the event",
+  },
+  USERS: {
+    // Write the initial error message constants for all the user-controllers here
+  },
+};
+
+export { DB_NAME, API_VERSION, CLOUDINARY_CLOUD_NAME, INITIAL_ERROR_MESSAGES };
